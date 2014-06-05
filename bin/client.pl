3e0aeaf9ddc85482b6bffb70c0b795a4c52d6419
Kamil <teodozjan@gmail.com>
2014-06-05 23:16:38 +0200

    Now chairman is satisfyinh

--- bin/client.pl
+++ bin/client.pl
@@ -35,12 +35,30 @@ class Client {
 	Transporter.new(bodybuilder => $!cache).transport_all_cargo;
     }
 
+
+#= chariman will upgrade only existing buildings but this may change in future
     submethod chairman {
 	$!cache = BodyBuilder.from_file('./var/bodybuilder.pl') unless $!cache;
 	my BuildGoal $wastet =  BuildGoal.new(building => wastetreatment, level=>15);
-	my BuildGoal $space =  BuildGoal.new(building => spaceport, level=>10);
-	my BuildGoal $arch =  BuildGoal.new(building => archaeology, level=>30);
-	my BuildGoal @goals = ($wastet,$space, $arch);
+	my BuildGoal $space .=  new(building => spaceport, level=>10);
+	my BuildGoal $arch .=  new(building => archaeology, level=>30);
+	my BuildGoal $sec .= new(building => security, level => 30); 
+
+
+	my BuildGoal $politic .= new(building => politicstraining,level => 15);
+	my BuildGoal $mayhem .= new(building => mayhemtraining, level => 15);
+	my BuildGoal $intel .= new(building => inteltraining, level => 15);
+	my BuildGoal $espionage .= new(building => espionage, level => 30);
+	my BuildGoal $intelli .= new(building => intelligence, level =>30);
+
+	my BuildGoal $happy .= new(building => entertainment, level => 30);
+
+	my BuildGoal @goals = (
+	    $wastet,$space, $arch, $sec,
+	    $politic, $mayhem, $intel, $espionage, $intelli,
+	    $happy
+	    );
+
 	my $c = GoalChairman.new(
 	    bodybuilder => $!cache,
 	    build_goals=>(@goals)	    
---
+++ lib/LacunaCookbuk/Logic/Chairman/Resource.pm
@@ -0,0 +1,5 @@
+use v6;
+
+#| Basic resources that regulate any planet int the empire
+enum Resource <food ore water energy waste happiness>;
+
