469641d5c300c46e6693f33e1d7b5db8819fdcfb
Kamil <teodozjan@gmail.com>
2014-06-10 03:50:55 +0200

    forgot hash

--- lib/LacunaCookbuk/Model/Building/Parliament.pm
+++ lib/LacunaCookbuk/Model/Building/Parliament.pm
@@ -6,6 +6,6 @@ class Parliament is LacunaBuilding;
 
 constant $URL = '/parliament';
 
-method view_propositions {
-    self.rpc($URL).view_propositions(self.session_id, self.id);
+method view_propositions returns Hash {
+    self.rpc($URL).view_propositions(self.session_id, self.id)<propositions>;
 }
